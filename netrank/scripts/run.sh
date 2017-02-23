9458a5af655c52e942dcd7893fc3a7dccb6036a4
Thibault Debatty <thibault.debatty@gmail.com>
2017-02-23 17:03:47 +0100

    Updated startup scripts

--- netrank/scripts/run.sh
+++ netrank/scripts/run.sh
@@ -3,13 +3,13 @@
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 
-
 NETRANK=`ls "$bin/../target/netrank-"*".jar" | sort -n | head -1`
 SERVER=`ls "$bin/../../server/target/server-"*".jar" | sort -n | head -1`
+LIBS="$bin/../../server/target/libs/"*
 
-# copy masfad2.jar to the modules directory, so the server can find it use the
+# copy netrank.jar to the modules directory, so the server can find it use the
 # masfad2 agents
 cp "$NETRANK" "$bin/modules/"
 
 # start the server with appropriate config file
-java -Xms1024m -jar "$SERVER" -c "$bin/config.yml"
+java -Xms1024m -cp "$LIBS:$SERVER" mark.server.Main -c "$bin/config.yml"
