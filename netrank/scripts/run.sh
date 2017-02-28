7e996e547858a463b4e6ae229b0a27bc459f0d48
Thibault Debatty <thibault.debatty@gmail.com>
2017-02-28 09:30:47 +0100

    Cleaned run script, maybe easier to run on windows?

--- netrank/scripts/run.sh
+++ netrank/scripts/run.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-bin=`dirname "$0"`
-bin=`cd "$bin"; pwd`
-
-NETRANK=`ls "$bin/../target/netrank-"*".jar" | sort -n | head -1`
-SERVER=`ls "$bin/../../server/target/server-"*".jar" | sort -n | head -1`
-LIBS="$bin/../../server/target/libs/"*
+ROOT=`dirname "$0"`
+ROOT=`cd "$ROOT"; pwd`
 
 # copy netrank.jar to the modules directory, so the server can find it use the
 # masfad2 agents
-cp "$NETRANK" "$bin/modules/"
+NETRANK=`ls "$ROOT/../target/netrank-"*".jar" | sort -n | head -1`
+rm "$ROOT/modules/"*".jar"
+cp "$NETRANK" "$ROOT/modules/"
+
+# Build classpath
+SERVER=`ls "$ROOT/../../server/target/server-"*".jar" | sort -n | head -1`
+CLASSPATH=$SERVER
+for f in $ROOT/../../server/target/libs/*.jar; do 
+  CLASSPATH=${CLASSPATH}:$f;
+done
 
 # start the server with appropriate config file
-java -Xms1024m -cp "$LIBS:$SERVER" mark.server.Main -c "$bin/config.yml"
+java -Xms1024m -cp "$CLASSPATH" mark.server.Main -c "$ROOT/config.yml"
