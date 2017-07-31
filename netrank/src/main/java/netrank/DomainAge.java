8f5616f50a7e8861d669d1f2593ac4b5513e9083
GeorgiNikolov <gnnikolov87@gmail.com>
2017-07-31 14:53:09 +0200

    Style corrections

--- netrank/src/main/java/netrank/DomainAge.java
+++ netrank/src/main/java/netrank/DomainAge.java
@@ -156,9 +156,9 @@ public class DomainAge implements DetectionAgentInterface<Link> {
         }
 
         Map<String, String> attributes = getAttributes(whois_result);
-        System.out.println("ATTRIBUTES: "+attributes);
+        //System.out.println("ATTRIBUTES: "+attributes);
         String creation_date = attributes.get(CREATION_DATE);
-        System.out.println("CREATION DATE: "+creation_date);
+        //System.out.println("CREATION DATE: "+creation_date);
         //String updated_date = attributes.get(UPDATED_DATE);
         //String expiration_date = attributes.get(EXPIRATION_DATE);
         DateFormat format = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
