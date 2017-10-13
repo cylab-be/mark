23a5abfd7915db9aa84f66de088d42fda8eb1bb7
GeorgiNikolov <gnnikolov87@gmail.com>
2017-10-13 15:19:06 +0200

    Added documentation to Evidence and RawData for the format of the time field. Added openjdk7 to the travis.yml file instead of oraclejdk7 as it is depricated

--- core/src/main/java/mark/core/Evidence.java
+++ core/src/main/java/mark/core/Evidence.java
@@ -8,6 +8,10 @@ public class Evidence<T extends Subject> {
 
     public String id = "";
     public String label = "";
+    /**
+     * The time variable is the timestamp attributed to the Evidence and is in 
+     * format: number of milliseconds since January 1, 1970, 00:00:00 GMT
+     */
     public long time;
     public T subject;
     public double score;
--- core/src/main/java/mark/core/RawData.java
+++ core/src/main/java/mark/core/RawData.java
@@ -6,6 +6,10 @@ package mark.core;
  */
 public class RawData<T extends Subject> {
     public String label = "";
+    /**
+     * The time variable is the timestamp attributed to the RawData and is in 
+     * format: number of milliseconds since January 1, 1970, 00:00:00 GMT
+     */
     public long time;
     public T subject;
     public String data = "";
