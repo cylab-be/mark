3327ebcb0fa5496add1474b67a5d957cd90dd334
GeorgiNikolov <gnnikolov87@gmail.com>
2017-12-06 14:23:06 +0100

    Better version of Email Dummy with no hardcoded string for the email generated

--- netrank/src/test/java/netrank/EmailDummyClient.java
+++ netrank/src/test/java/netrank/EmailDummyClient.java
@@ -23,6 +23,10 @@
  */
 package netrank;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Random;
 import mark.activation.DummyClient;
@@ -42,56 +46,20 @@ public class EmailDummyClient<T extends Subject> extends DummyClient<T> {
     // Simulate an APT that connects every 60 seconds => f = 0.0166 Hz
     private static final int APT_INTERVAL = 60;
     private LinkedList<Evidence> evidence = new LinkedList<>();
-    String email_Mime = "MIME-Version: 1.0\n" +
-"Content-Type: multipart/alternative; boundary=\"outer-boundary\"\n" +
-"\n" +
-"This is a MIME-encoded message. If you are seeing this, your mail\n" +
-"reader is old.\n" +
-"--outer-boundary\n" +
-"Content-Type: text/plain; charset=us-ascii\n" +
-"\n" +
-"This message might make you :) or it might make you :(\n" +
-"\n" +
-"--outer-boundary\n" +
-"MIME-Version: 1.0\n" +
-"Content-Type: multipart/related;\n" +
-"  type=\"text/html\"; start=\"<body@here>\"; boundary=\"inner-boundary\"\n" +
-"\n" +
-"--inner-boundary\n" +
-"Content-Type: text/html; charset=us-ascii\n" +
-"Content-Disposition: inline\n" +
-"Content-ID: <body@here>\n" +
-"\n" +
-"<html>\n" +
-" <body>\n" +
-"  This message might make you\n" +
-"  <img src=\"cid:smile@here\" alt=\"smile\">\n" +
-"  or it might make you\n" +
-"  <img src=\"cid:frown@here\" alt=\"frown\">\n" +
-" </body>\n" +
-"</html>\n" +
-"\n" +
-"--inner-boundary\n" +
-"Content-Type: image/gif\n" +
-"Content-Disposition: inline\n" +
-"Content-Transfer-Encoding: base64\n" +
-"Content-ID: <smile@here>\n" +
-"\n" +
-"R0lGODlhEAAQAKEBAAAAAP//AP//AP//ACH5BAEKAAIALAAAAAAQABAAAAIzlA2px6IBw2\n" +
-"IpWglOvTahDgGdI0ZlGW5meKlci6JrasrqkypxJr8S0oNpgqkGLtcY6hoFADs=\n" +
-"\n" +
-"--inner-boundary\n" +
-"Content-Type: image/gif\n" +
-"Content-Disposition: inline\n" +
-"Content-Transfer-Encoding: base64\n" +
-"Content-ID: <frown@here>\n" +
-"\n" +
-"R0lGODlhEAAQAKEBAAAAAAD//wD//wD//yH5BAEKAAIALAAAAAAQABAAAAIzlA2px6IBw2\n" +
-"IpWglOvTahDgGdI0ZlGW5meKlci75drDzm5uLZyZ1I3Mv8ZB5Krtgg1RoFADs=\n" +
-"\n" +
-"--inner-boundary--\n" +
-"\n" +
-"--outer-boundary--";
+    String email_Mime = "";
+
+    public void loadMIME() throws FileNotFoundException, IOException {
+        String path = getClass().getResource("/MIME.txt")
+                        .getPath();
+        FileReader file = new FileReader(path);
+        BufferedReader reader = new BufferedReader(file);
+        String line = reader.readLine();
+
+        while(line != null) {
+            email_Mime += line;
+            line = reader.readLine();
+        }
+    }
 
     private RawData[] generateData(String type, Link subject) {
         RawData[] data = new RawData[N];
