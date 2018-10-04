f64763bb76caed4706d330983f69e46ada966eba
Georgi Nikolov <gnnikolov87@gmail.com>
2018-10-04 11:26:27 +0200

    Added an ArchiveSource class in netrack as datasource when parsing tar.gz archives. Added a test in Integration to test that it works well and adapted the pom with the needed dependencies

---
+++ integration/src/test/java/HTTPArchiveSourceIT.java
@@ -0,0 +1,77 @@
+
+import java.util.HashMap;
+import junit.framework.TestCase;
+import mark.core.DataAgentProfile;
+import mark.server.Config;
+import mark.server.Server;
+import netrank.ArchiveSource;
+import netrank.LinkAdapter;
+
+/*
+ * The MIT License
+ *
+ * Copyright 2018 georgi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ *
+ * @author georgi
+ */
+public class HTTPArchiveSourceIT extends TestCase {
+    private Server server;
+
+    @Override
+    protected final void tearDown() throws Exception {
+        server.stop();
+        super.tearDown();
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    public final void testHTTPArchiveSource() throws Throwable {
+
+        System.out.println("Test with a HTTP archive source");
+        System.out.println("============================");
+
+        Config config = Config.getTestConfig();
+        config.adapter_class = LinkAdapter.class.getName();
+        server = new Server(config);
+
+        // Configure a single data source (HTTP, Regex, file with 1000 reqs)
+        HashMap<String, String> parameters = new HashMap<String, String>();
+        parameters.put(
+                "file",
+                getClass().getResource("/example_json_proxy.gz").getPath());
+
+        DataAgentProfile example_json_proxy = new DataAgentProfile();
+        example_json_proxy.class_name = ArchiveSource.class.getCanonicalName();
+        example_json_proxy.parameters = parameters;
+        example_json_proxy.label = "data.http";
+        server.addDataAgentProfile(example_json_proxy);
+
+        server.start();
+
+        // Wait for data sources and detection to finish...
+        server.awaitTermination();
+    }
+}
---
+++ netrank/src/main/java/netrank/ArchiveSource.java
@@ -0,0 +1,188 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 georgi.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package netrank;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import mark.core.DataAgentInterface;
+import mark.core.DataAgentProfile;
+import mark.core.ServerInterface;
+import java.util.zip.GZIPInputStream;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import mark.core.RawData;
+
+/**
+ *
+ * A generic data agent that reads an archive of files, extracts it and parses
+ * rthe files (in JSON format).
+ * This data agent is usually used for testing detection agents, or to run a
+ * demo.
+ * @author Georgi Nikolov
+ */
+public class ArchiveSource implements DataAgentInterface {
+
+    /**
+     * Use this key in the parameters map to set the speedup for reading the
+     * file.
+     */
+    public static final String SPEEDUP_KEY = "speedup";
+
+    private static final double DEFAULT_SPEEDUP = Double.MAX_VALUE;
+
+    /**
+     *
+     * @param profile
+     * @param datastore
+     * @throws Throwable if something went wrong...
+     */
+    public final void run(
+            final DataAgentProfile profile, final ServerInterface datastore)
+            throws Throwable {
+
+        double speedup = DEFAULT_SPEEDUP;
+        String speedup_string = profile.parameters.get(SPEEDUP_KEY);
+        if (speedup_string != null) {
+            try {
+                speedup = Double.valueOf(speedup_string);
+            } catch (NumberFormatException ex) {
+                speedup = DEFAULT_SPEEDUP;
+            }
+        }
+
+        long start_time = 0;
+        long first_data_time = 0;
+
+        byte[] buffer = new byte[1024];
+
+        try {
+
+            FileInputStream file_in =
+                    new FileInputStream(profile.parameters.get("file"));
+
+            GZIPInputStream gzip_input_stream = new GZIPInputStream(file_in);
+
+            StringBuffer str = new StringBuffer();
+            int bytes_read;
+            while ((bytes_read = gzip_input_stream.read(buffer)) > 0) {
+                if (Thread.currentThread().isInterrupted()) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+
+                String s = new String(buffer, 0, bytes_read);
+                str.append(s);
+            }
+
+            gzip_input_stream.close();
+
+            System.out.println("The file was decompressed successfully!");
+            String final_string = str.toString();
+            String[] lines = final_string.split("\\r?\\n");
+            for (int i = 0; i < lines.length; i++) {
+                RawData rd = parseLine(lines[i]);
+                rd.label = profile.label;
+
+                if (start_time == 0) {
+                    start_time = System.currentTimeMillis();
+                    first_data_time = rd.time;
+                }
+
+                // Simulated time for this new data
+                rd.time = rd.time - first_data_time + start_time;
+
+                long wait_time = (long) ((rd.time - start_time) / speedup);
+                Thread.sleep(wait_time);
+
+                datastore.addRawData(rd);
+            }
+            // Print some stats
+            System.out.println("----");
+            System.out.println("Number of lines: " + lines.length);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private RawData parseLine(final String line) throws Exception {
+        RawData rd = new RawData();
+        JsonParser parser = new JsonParser();
+        JsonObject json_obj = (JsonObject) parser.parse(line);
+        try {
+            SimpleDateFormat sdf
+                    = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date date = sdf.parse(json_obj.get("@timestamp").getAsString());
+            Long timestamp = date.getTime();
+
+            String client = "unkown";
+            if (json_obj.has("tk_client_ip")) {
+                client = json_obj.get("tk_client_ip").getAsString();
+            }
+            String method = "unkown";
+            if (json_obj.has("tk_operation")) {
+                method = json_obj.get("tk_operation").getAsString();
+            }
+            int bytes = 0;
+            if (json_obj.has("tk_size")) {
+                bytes = json_obj.get("tk_size").getAsInt();
+            }
+            String url = "unkown";
+            if (json_obj.has("tk_url")) {
+                url = json_obj.get("tk_url").getAsString();
+            }
+            String peerhost = "unkown";
+            if (json_obj.has("tk_server_ip")) {
+                peerhost = json_obj.get("tk_server_ip").getAsString();
+            }
+            String type = "unkown";
+            if (json_obj.has("tk_mime_content")) {
+                type = json_obj.get("tk_mime_content").getAsString();
+            }
+
+            String raw_data = timestamp
+                    + " " + 0 //info elapsed time not available
+                    + " " + client //client IP
+                    + " " + "unknown" //code status not available
+                    + " " + bytes //bytes sent/recieved
+                    + " " + method //method of connection
+                    + " " + url //destination url
+                    + " " + "-"
+                    + " " + "DIRECT/" + peerhost //server IP
+                    + " " + type; //mime type of connection
+
+            rd.time = timestamp;
+            rd.data = raw_data;
+            rd.subject = new Link(client, peerhost);
+        } catch (ParseException ex) {
+            System.out.println("Error Parsing JSON: " + ex);
+        }
+        return rd;
+    }
+}
