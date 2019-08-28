6dc15a3fb2ac6a39aeead6abd4f9d650c3d9572b
Thibault Debatty <thibault.debatty@gmail.com>
2019-08-28 09:46:37 +0200

    do NOT open browser at start. Fix #35

--- server/src/main/java/mark/server/Main.java
+++ server/src/main/java/mark/server/Main.java
@@ -69,28 +69,5 @@ public final class Main {
         });
 
         server.start();
-
-        // Launch browser
-        String url = "http://127.0.0.1:8000";
-
-        if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-            try {
-                desktop.browse(new URI(url));
-
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        } else {
-            Runtime runtime = Runtime.getRuntime();
-            try {
-                runtime.exec("xdg-open " + url);
-
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
     }
 }
