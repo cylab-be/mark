6bf46347b3d4d24aeaf868514f1443c775302319
Thibault Debatty <thibault.debatty@gmail.com>
2019-08-30 16:10:17 +0200

    Remove PHP code and add basic Spark web server

--- server/src/scripts/run.sh
+++ server/src/scripts/run.sh
@@ -6,4 +6,4 @@ ROOT=`cd "$ROOT"; pwd`
 SERVER=`ls "$ROOT/bin/server-"*".jar" | sort -n | head -1`
 LIBS="$ROOT/libs/*"
 
-java -Xms1024m -cp "$LIBS:$SERVER" mark.server.Main -c "$ROOT/config.yml"
+java -Xms1024m -cp "$LIBS:$SERVER" be.cylab.mark.server.Main -c "$ROOT/config.yml"
