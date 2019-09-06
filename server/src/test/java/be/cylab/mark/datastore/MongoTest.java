4559901b8bea0f47c20d92b59b9eaeb4d5c77bde
Thibault Debatty <thibault.debatty@gmail.com>
2019-09-06 11:10:59 +0200

    Add basic mongodb tests

--- server/src/test/java/be/cylab/mark/datastore/MongoTest.java
+++ server/src/test/java/be/cylab/mark/datastore/MongoTest.java
@@ -24,25 +24,62 @@
 
 package be.cylab.mark.datastore;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import java.util.List;
 import junit.framework.TestCase;
-import be.cylab.mark.server.Config;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 /**
  *
  * @author Thibault Debatty
  */
-public class DatastoreTest extends TestCase {
+public class MongoTest extends TestCase {
 
-    public DatastoreTest(String testName) {
-        super(testName);
+    public static void Main() {
+        MongoTest test = new MongoTest();
+        test.testPojo();
     }
 
-    public void testStartDatastore() throws Exception {
-        Config conf = Config.getTestConfig();
+    public void testPojo() {
 
-        Datastore datastore = new Datastore(conf, new DummyActivationContoller());
-        datastore.start();
-        Thread.sleep(1000);
-        datastore.stop();
+        CodecRegistry pojoCodecRegistry = fromRegistries(
+                MongoClientSettings.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+        .codecRegistry(pojoCodecRegistry)
+        .build();
+
+        MongoClient mongo = MongoClients.create(settings);
+        MongoDatabase db = mongo.getDatabase("mytestdb");
+
+        MongoCollection<Person> collection =
+                db.getCollection("people", Person.class);
+        collection.drop();
+
+        Person me = new Person();
+        me.setName("Tibo");
+
+        // a simple array of String is not serialized automatically...
+        // => use a list
+        List<String> refs = new ArrayList<>();
+        refs.add("abc");
+        refs.add("def");
+        me.setReferences(refs);
+        collection.insertOne(me);
+
+        System.out.println("Mutated Person Model: " + me);
+
+        Person first = collection.find().first();
+        System.out.println("In MongoDB: " + first);
+
     }
 }
---
+++ server/src/test/java/be/cylab/mark/datastore/Person.java
@@ -0,0 +1,86 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 tibo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package be.cylab.mark.datastore;
+
+import java.util.Arrays;
+import java.util.List;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author tibo
+ */
+public final class Person {
+    private ObjectId id;
+    private String name;
+    private Address address;
+    public List<String> references;
+
+    public Person() {
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(final ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(final Address address) {
+        this.address = address;
+    }
+
+    public List<String> getReferences() {
+        return references;
+    }
+
+    public void setReferences(List<String> references) {
+        this.references = references;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "id='" + id + "'"
+                + ", name='" + name + "'"
+                + ", address=" + address
+                + ", references=" + references
+                + "}";
+    }
+}
